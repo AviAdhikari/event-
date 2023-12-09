38398d6328b7d5fc5656ff0c374a36750ba7fd76
AviAdhikari <aviadhikari671@gmail.com>
2023-12-09 21:33:49 +0545

    inital commit

---
+++ home/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+# In your urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+    
+
+
+urlpatterns = [
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("services", views.services, name='services'),
+    path("gallery", views.gallery, name='gallery'),
+    path("contactus", views.contact, name='bar'),
+    path("login", views.login, name='login'),
+    path("register", views.register, name='register'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'aboutus.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request,'contactus.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
